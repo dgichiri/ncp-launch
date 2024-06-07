55f520e9227d942068bcca817131755bb1eea93e
dgichiri <dgichiri@gmail.com>
2024-06-07 12:02:30 +0300

    Update BMCascade.py

--- BMCascade.py
+++ BMCascade.py
@@ -10,15 +10,15 @@ import random
 
 st.write("""
  
-# Welcome to B&M Mission cascade
+# Welcome to Shine Team Kenya Dinner
         
          """)
 
-msgs = ['Your tech insights are like hidden gems in the digital finance landscape.',
-        'Your determination in tackling tech challenges is truly inspiring',
+msgs = ['Just dropping here to whisper this encouragement; may your journey be filled with the same spirit of unity and sportsmanship found in the Olympic Games. Go for gold!',
+        'Please receive this, our wish to you; just like an Olympian, you're fuelled by passion and driven by dedication. Keep pushing forward and making your family, friends, and colleagues proud!',
         'Your dedication to excellence is like a heavenly virtue, infusing our projects with the essence of success and achievement',
-        'Your tech skills are like the backbone of our digital infrastructure, providing stability and reliability in an ever-changing landscape',
-        'You demonstrates a high level of creativity in developing innovative brand communication strategies',
+        'The heart of a champion! So, we thought we remind you; you can inspire those around you to be their best. Keep spreading your Olympic spirit!',
+        'We thought you should know, that just like an Olympian, you can inspire greatness in everything you do, and in everyone around you. Keep reaching for your dreams!',
         'Your tech insights are like guiding stars in the digital universe, illuminating paths to innovation and progress. Keep shining bright and leading the way',
         'Your determination in the face of technical challenges is like a force of nature. Your resilience and tenacity propel us forward, overcoming obstacles and achieving greatness.',
         'Your tech expertise is like a cornerstone, providing stability and strength to our digital endeavors. Keep building and fortifying our technological foundations!',
